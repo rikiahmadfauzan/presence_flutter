f8397810f3612fec277bec5ff0ee1fc400d266ce
iki <111642344+rikiahmadfauzan@users.noreply.github.com>
2023-09-14 10:41:16 +0700

    Push Commit
    Push

--- lib/app/modules/all_presensi/controllers/all_presensi_controller.dart
+++ lib/app/modules/all_presensi/controllers/all_presensi_controller.dart
@@ -1,23 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 class AllPresensiController extends GetxController {
+  DateTime? start;
+  DateTime end = DateTime.now();
+
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> streamAllPresence() async* {
+  Future<QuerySnapshot<Map<String, dynamic>>> getPresence() async {
     String uid = auth.currentUser!.uid;
+    print(start);
+    print(end);
 
-    String todayID =
-        DateFormat.yMd().format(DateTime.now()).replaceAll("/", "-");
+    if (start == null) {
+      return await firestore
+          .collection("pegawai")
+          .doc(uid)
+          .collection("presence")
+          .where("date", isLessThan: end.toIso8601String())
+          .orderBy("date", descending: true)
+          .get();
+    } else {
+      return await firestore
+          .collection("pegawai")
+          .doc(uid)
+          .collection("presence")
+          .where("date", isGreaterThan: start!.toIso8601String())
+          .where("date",
+              isLessThan: end.add(Duration(days: 1)).toIso8601String())
+          .orderBy("date", descending: true)
+          .get();
+    }
+  }
 
-    yield* firestore
-        .collection("pegawai")
-        .doc(uid)
-        .collection("presence")
-        .orderBy("date", descending: true)
-        .snapshots();
+  void pickDate(DateTime pickStart, DateTime pickEnd) {
+    start = pickStart;
+    end = pickEnd;
+    update();
+    Get.back();
   }
 }
--- lib/app/modules/all_presensi/views/all_presensi_view.dart
+++ lib/app/modules/all_presensi/views/all_presensi_view.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import '../../../routes/app_pages.dart';
 import '../controllers/all_presensi_controller.dart';
 
@@ -16,90 +15,113 @@ class AllPresensiView extends GetView<AllPresensiController> {
         title: const Text('ALL PRESENSI'),
         centerTitle: true,
       ),
-      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: controller.streamAllPresence(),
-        builder: (context, snap) {
-          if (snap.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snap.data?.docs.length == 0 || snap.data == null) {
-            return SizedBox(
-              height: 200,
-              child: Center(
-                child: Text("Belum ada history..."),
-              ),
-            );
-          }
-          return ListView.builder(
-            padding: EdgeInsets.all(20),
-            itemCount: snap.data!.docs.length,
-            itemBuilder: (context, index) {
-              Map<String, dynamic> data = snap.data!.docs[index].data();
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Material(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey[200],
-                  child: InkWell(
+      body: GetBuilder<AllPresensiController>(
+        builder: (c) => FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          future: controller.getPresence(),
+          builder: (context, snap) {
+            if (snap.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snap.data?.docs.length == 0 || snap.data == null) {
+              return SizedBox(
+                height: 150,
+                child: Center(
+                  child: Text("Belum ada history..."),
+                ),
+              );
+            }
+            return ListView.builder(
+              padding: EdgeInsets.all(20),
+              itemCount: snap.data!.docs.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> data = snap.data!.docs[index].data();
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Material(
                     borderRadius: BorderRadius.circular(20),
-                    onTap: () => Get.toNamed(
-                      Routes.DETAIL_PRESENSI,
-                      arguments: data,
-                    ),
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey[200],
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () => Get.toNamed(
+                        Routes.DETAIL_PRESENSI,
+                        arguments: data,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Masuk",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Masuk",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                "${DateFormat.yMMMEd().format(DateTime.parse(data['date']))}",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+                                Text(
+                                  "${DateFormat.yMMMEd().format(DateTime.parse(data['date']))}",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Text(data['masuk']?['date'] == null
-                              ? "-"
-                              : "${DateFormat.jms().format(DateTime.parse(data['masuk']!['date']))}"),
-                          Text(
-                            "Keluar",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
+                              ],
                             ),
-                          ),
-                          Text(data['keluar']?['date'] == null
-                              ? "-"
-                              : "${DateFormat.jms().format(DateTime.parse(data['keluar']!['date']))}"),
-                        ],
+                            Text(data['masuk']?['date'] == null
+                                ? "-"
+                                : "${DateFormat.jms().format(DateTime.parse(data['masuk']!['date']))}"),
+                            Text(
+                              "Keluar",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(data['keluar']?['date'] == null
+                                ? "-"
+                                : "${DateFormat.jms().format(DateTime.parse(data['keluar']!['date']))}"),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              );
-            },
-          );
-        },
+                );
+              },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // sying fct
+          Get.dialog(
+            Dialog(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                height: 400,
+                child: SfDateRangePicker(
+                  monthViewSettings:
+                      DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
+                  selectionMode: DateRangePickerSelectionMode.range,
+                  showActionButtons: true,
+                  onCancel: () => Get.back(),
+                  onSubmit: (obj) {
+                    if (obj != null) {
+                      if ((obj as PickerDateRange).endDate != null) {
+                        controller.pickDate(obj.startDate!, obj.endDate!);
+                      }
+                    }
+                  },
+                ),
+              ),
+            ),
+          );
         },
-        child: Icon(Icons.format_list_bulleted_rounded),
+        child: const Icon(Icons.format_list_bulleted_rounded),
       ),
     );
   }
